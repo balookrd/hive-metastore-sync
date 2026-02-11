be279305bd541d4140c30287346090bbedceaae7
Малыхин Максим Владиславович <mmalykhin@beeline.ru>
2026-02-11 20:39:00 +0300

    fix: some refactoring

--- src/com/wandisco/hivesync/main/Main.java
+++ src/com/wandisco/hivesync/main/Main.java
@@ -1,47 +1,42 @@
 package com.wandisco.hivesync.main;
 
-import java.io.File;
-
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
 import com.wandisco.hivesync.hive.Commands;
 
+import java.io.File;
+
 /**
- * 
+ *
  * @author Oleg Danilov
- * 
+ *
  */
 public class Main {
+    public static void main(String[] args) throws Exception {
+        Params p = new Params();
+        JCommander jce = new JCommander(p);
+        try {
+            jce.parse(args);
+        } catch (ParameterException e) {
+            System.err.println("ERROR: " + e.getMessage());
+            jce.usage();
+            System.exit(1);
+        }
 
-  private static Params p;
+        if (p.getHelp() != null) {
+            jce.setProgramName("hive-metastore-sync");
+            jce.usage();
+            return;
+        }
 
-  public static void main(String[] args) throws Exception {
-    p = new Params();
-    JCommander jce = new JCommander(p);
-    try {
-      jce.parse(args);
-    } catch (ParameterException e) {
-      System.err.println("ERROR: " + e.getMessage());
-      jce.usage();
-      System.exit(1);
+        // Delete dryRun file
+        String dryRunFile = p.getDryRunFile();
+        Commands.setDryRunFile(dryRunFile);
+        if (dryRunFile != null) {
+            (new File(dryRunFile)).delete();
+        }
+
+        HiveSync hs = new HiveSync(p.getSrc(), p.getDst(), p.getDatabases());
+        hs.execute();
     }
-
-    if (p.getHelp() != null) {
-      jce.setProgramName("hive-metastore-sync");
-      jce.usage();
-      return;
-    }
-
-    // Delete dryRun file
-    String dryRunFile = p.getDryRunFile();
-    Commands.setDryRunFile(dryRunFile);
-    if (dryRunFile != null) {
-      (new File(dryRunFile)).delete();
-    }
-
-    HiveSync hs = new HiveSync(p.getSrc(), p.getSrcUser(), p.getSrcPass(), p.getDst(),
-        p.getDstUser(), p.getDstPass(), p.getDatabases());
-    hs.execute();
-  }
-
 }
